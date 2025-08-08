df2d38076a59a34a675d1ba210a8694e34c7c936
RozanRamadani <114994099+RozanRamadani@users.noreply.github.com>
2025-08-08 10:55:09 +0700

    Auth with Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken($request->device_name)->plainTextToken;
+    }
+
+    function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out successfully.']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
 
-Route::post('/todos', [TodoController::class, 'store']);
-Route::patch('/todos/{id}', [TodoController::class, 'update']);
-Route::delete('/todos/{id}', [TodoController::class, 'delete']);
+Route::post('/todos', [TodoController::class, 'store'])->middleware('auth:sanctum');
+Route::patch('/todos/{id}', [TodoController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/todos/{id}', [TodoController::class, 'delete'])->middleware('auth:sanctum');
 Route::get('/todos', [TodoController::class, 'index']);
 Route::get('/todos/{id}', [TodoController::class, 'show']);
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
